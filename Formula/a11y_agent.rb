72893ec3b12bf7c327e5487830c4814210b0b82e
Cameron Cundiff <cameroncundiff@asana.com>
2024-07-28 13:38:00 -0400

    v0.0.3

--- Formula/a11y_agent.rb
+++ Formula/a11y_agent.rb
@@ -1,8 +1,8 @@
 class A11yAgent < Formula
   desc "AI agent that fixes accessibility issues"
   homepage "https://github.com/accesslint/a11y-agent"
-  url "https://github.com/AccessLint/a11y-agent/releases/download/v0.0.2/a11y_agent-0.0.2.tar.xz"
-  sha256 "c957960a618dc1542075fb54dbf74d28cf248132d18dd96db94461db3d63a906"
+  url "https://github.com/AccessLint/a11y-agent/releases/download/v0.0.3/a11y_agent-0.0.3.tar.xz"
+  sha256 "6b01bd85fd5e764fd38595d305442d23afb40eff7357cba7b1c229fb9afc366a"
 
   depends_on "ruby"
 
