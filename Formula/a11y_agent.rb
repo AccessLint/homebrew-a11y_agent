ca8d0531cb3d3490adfb0ed7e81190e4a0d9e348
Cameron Cundiff <github@ckundo.com>
2024-08-18 12:43:31 -0400

    v0.0.7

--- Formula/a11y_agent.rb
+++ Formula/a11y_agent.rb
@@ -1,8 +1,8 @@
 class A11yAgent < Formula
   desc "AI agent that fixes accessibility issues"
   homepage "https://github.com/accesslint/a11y-agent"
-  url "https://github.com/AccessLint/a11y-agent/releases/download/v0.0.6/a11y_agent-0.0.6.tar.xz"
-  sha256 "0d49b9644364bf06d2eb4f67b358da9f3f2988a68504a314e280efd0ca460ec8"
+  url "https://github.com/AccessLint/a11y-agent/releases/download/v0.0.7/a11y_agent-0.0.7.tar.xz"
+  sha256 "8c28235183fed93166510a2923f113903459f50f3137969ad182178fc307ef73"
 
   depends_on "ruby"
 
@@ -11,9 +11,6 @@ class A11yAgent < Formula
 
     # Extract all files to libexec, which is a common Homebrew practice for third-party tools
     libexec.install Dir["*"]
-
-    system "gem", "build", "a11y_agent.gemspec"
-    system "gem", "install", "--ignore-dependencies", "a11y_agent-0.0.6.gem"
 
     bin.install libexec/"exe/a11y_agent"
     bin.env_script_all_files(libexec/"exe", GEM_HOME: ENV.fetch("GEM_HOME", nil))
